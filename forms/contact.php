d8ce19600c14baa60a41c5dab1088d02855e74d8
Nigel <nigeltakadza@gmail.com>
2023-07-27 12:13:16 +0200

    added new package

--- forms/contact.php
+++ forms/contact.php
@@ -1,18 +1,34 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $subject = $_POST['subject'];
-  $message = $_POST['message'];
 
-  $to = 'heinrich@wellnessexcellence.co.za'; // Change this to your own email address
-  $headers = "From: $name <$email>";
-  $body = "Name: $name\nEmail: $email\nSubject: $subject\n\n$message";
+  $receiving_email_address = 'heinrich@wellnessexcellence.co.za';
 
-  if (mail($to, $subject, $body, $headers)) {
-    echo "success";
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
   } else {
-    echo "error";
+    die( 'Unable to load the "PHP Email Form" Library!');
   }
-}
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
+
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  /*
+  $contact->smtp = array(
+    'host' => 'example.com',
+    'username' => 'example',
+    'password' => 'pass',
+    'port' => '587'
+  );
+  */
+
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
+
+  echo $contact->send();
 ?>
